05f53748c4e925efaa9250e8761b5cd6f0468886
Omar Saul Morales Ibarra <omar.morales@lurn.com>
2022-08-18 13:20:53 -0500

    adding cart context, shopping icon and cart dropdown

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from 'react'
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
 
-// actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
