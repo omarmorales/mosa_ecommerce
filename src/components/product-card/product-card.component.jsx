8d0e563002205c664d2f3fae37d24e8876757019
Omar Saul Morales Ibarra <omar.morales@lurn.com>
2022-08-26 17:33:43 -0500

    styled components added to project

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,22 +1,27 @@
-import { useContext } from 'react';
-import Button from '../button/button.component';
-import { CartContext } from '../../contexts/cart.context';
-import './product-card.styles.scss'
+import { useContext } from "react";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
+import { CartContext } from "../../contexts/cart.context";
+import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
-  const { addItemToCart } = useContext(CartContext)
-  const addProductToCart = () => addItemToCart(product)
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
   return (
-    <div className='product-card-container'>
+    <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
-      <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
+        Add to card
+      </Button>
     </div>
-  )
-}
+  );
+};
 
 export default ProductCard;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,42 +1,45 @@
-import { Fragment, useContext } from "react"
-import { Outlet, Link } from "react-router-dom"
+import { Fragment, useContext } from "react";
+import { Outlet } from "react-router-dom";
 
-import { ReactComponent as CrownLogo } from "../../assets/crown.svg"
-import CartIcon from "../../components/cart-icon/cart-icon.component"
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component"
-import { UserContext } from "../../contexts/user.context"
-import { CartContext } from "../../contexts/cart.context"
-import { signOutUser } from "../../utils/firebase/firebase.utils"
-import "./navigation.styles.scss"
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from "./navigation.styles";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext)
-  const { isCartOpen } = useContext(CartContext)
+  const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrownLogo className="logo" />
-        </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
+            <NavLink as="span" onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
-              SIGN IN
-            </Link>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
           <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
